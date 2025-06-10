ded72cc25534604f9c888742e060d19e3112c14e
zeed200 <aaljindary@gmail.com>
2025-06-10 08:51:37 +0300

    update model

--- bookmark/models.py
+++ bookmark/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class source(models.Model):
     title = models.CharField(max_length=200)
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=500)
     note = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
